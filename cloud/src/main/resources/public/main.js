c4be63c4cca1bc209164b50df3eadd532dd1d5d0
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2021-04-02 15:52:48 +0200

    added initial basic gui..

---
+++ cloud/src/main/resources/public/main.js
@@ -0,0 +1,86 @@
+const config = {
+    'host': '192.168.1.101',
+    'service_port': 7000,
+    'websocket_port': 8884
+};
+
+$(window).on('load', function () {
+    const email = localStorage.getItem('email');
+    const password = localStorage.getItem('password');
+    if (email && password) {
+        const form = new FormData();
+        form.append('email', email);
+        form.append('password', password);
+        fetch('http://' + config.host + ':' + config.service_port + '/login', {
+            method: 'post',
+            body: form
+        }).then(response => {
+            if (response.ok) {
+                response.json().then(data => { setUser(data); });
+            } else {
+                localStorage.removeItem('email');
+                localStorage.removeItem('password');
+                $.get('body_guest.html', function (data) { $('#sirobotics-body').append(data); });
+            }
+        });
+    } else {
+        $.get('body_guest.html', function (data) { $('#sirobotics-body').append(data); });
+    }
+});
+
+function setUser(user) {
+
+}
+
+
+function login() {
+    const email = $('#login-email').val();
+    const password = $('#login-password').val();
+    const form = new FormData();
+    form.append('email', email);
+    form.append('password', password);
+    fetch('http://' + config.host + ':' + config.service_port + '/login', {
+        method: 'post',
+        body: form
+    }).then(response => {
+        if (response.ok) {
+            localStorage.setItem('email', email);
+            localStorage.setItem('password', password);
+            location.reload();
+        } else
+            alert(response.statusText);
+    });
+}
+
+function logout() {
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
+    location.reload();
+}
+
+function signin() {
+    const email = $('#signin-email').val();
+    const password = $('#signin-password').val();
+    const first_name = $('#signin-first-name').val();
+    const last_name = $('#signin-last-name').val();
+    const form = new FormData();
+    form.append('email', email);
+    form.append('password', password);
+    form.append('first_name', first_name);
+    form.append('last_name', last_name);
+    form.append('email', email);
+    form.append('password', password);
+    form.append('first_name', first_name);
+    form.append('last_name', last_name);
+    fetch('http://' + config.host + ':' + config.service_port + '/user', {
+        method: 'post',
+        body: form
+    }).then(response => {
+        if (response.ok) {
+            localStorage.setItem('email', email);
+            localStorage.setItem('password', password);
+            location.reload();
+        } else
+            alert(response.statusText);
+    });
+}
