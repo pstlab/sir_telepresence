46314eebe2eb56c28591590faaba052bf7933f05
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2021-06-02 18:08:22 +0200

    improved dialogue management..

--- nlu/actions/actions.py
+++ nlu/actions/actions.py
@@ -1,27 +1,25 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import logging
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionCommandStart(Action):
+    def name(self) -> Text:
+        return 'action_command_start'
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        command_id = next(tracker.get_latest_entity_values('number'), None)
+        logging.info('starting command id: ' + str(command_id))
+        return [SlotSet('command_id', command_id)]
+
+
+class ActionCommandDone(Action):
+    def name(self) -> Text:
+        return 'action_command_done'
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        command_id = tracker.get_slot('command_id')
+        logging.info('ending command id: ' + str(command_id))
+        return []
