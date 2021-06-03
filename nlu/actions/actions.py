badd4c1c2063acff274793e3c46cdf04188633b7
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2021-06-03 10:33:28 +0200

    cleaning interaction..

--- nlu/actions/actions.py
+++ nlu/actions/actions.py
@@ -2,7 +2,29 @@ from typing import Any, Text, Dict, List
 from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import paho.mqtt.client as mqtt
 import logging
+
+
+def on_connect(client, userdata, flags, rc):
+    logging.info('Connected with result code ' + str(rc))
+
+
+client = mqtt.Client('RASA Actions')
+client.on_connect = on_connect
+client.connect('localhost')
+client.loop_forever()
+
+
+class ActionSetPrefix(Action):
+    def name(self) -> Text:
+        return 'action_mqtt_prefix'
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        prefix = next(tracker.get_latest_entity_values('prefix'), None)
+        prefix = prefix.replace('prefix:', '')
+        logging.info('MQTT prefix: ' + prefix)
+        return [SlotSet('mqtt_prefix', prefix)]
 
 
 class ActionCommandStart(Action):
@@ -10,9 +32,10 @@ class ActionCommandStart(Action):
         return 'action_command_start'
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        command_id = next(tracker.get_latest_entity_values('number'), None)
-        logging.info('starting command id: ' + str(command_id))
-        return [SlotSet('command_id', command_id)]
+        sigma = next(tracker.get_latest_entity_values('sigma'), None)
+        sigma = sigma.replace('sigma:', '')
+        logging.info('starting command sigma: ' + sigma)
+        return [SlotSet('sigma', sigma)]
 
 
 class ActionCommandDone(Action):
@@ -20,6 +43,20 @@ class ActionCommandDone(Action):
         return 'action_command_done'
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        command_id = tracker.get_slot('command_id')
-        logging.info('ending command id: ' + str(command_id))
+        prefix = tracker.get_slot('mqtt_prefix')
+        sigma = tracker.get_slot('sigma')
+        logging.info('ending command sigma: ' + sigma)
+        client.publish(prefix + '\done', '[' + sigma + ']')
+        return []
+
+
+class ActionCommandFailure(Action):
+    def name(self) -> Text:
+        return 'action_command_failure'
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        prefix = tracker.get_slot('mqtt_prefix')
+        sigma = tracker.get_slot('sigma')
+        logging.info('failing command sigma: ' + sigma)
+        client.publish(prefix + '\failure', '[' + sigma + ']')
         return []
