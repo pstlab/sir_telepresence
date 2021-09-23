2f57f0b74ff6b91b43462547e02d25478378b658
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2021-09-23 09:46:34 +0200

    major refactoring..

--- edge/src/main/java/it/cnr/istc/pst/sirobotics/edge/api/DeviceTypeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package it.cnr.istc.pst.sirobotics.edge.api;
-
-class DeviceTypeNotFoundException extends RuntimeException {
-
-    DeviceTypeNotFoundException(Long id) {
-        super("Could not find device type " + id);
-    }
-}
