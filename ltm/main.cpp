d36296344cc56e8e8066b12f7929b996fee5c4ce
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2021-09-24 18:08:56 +0200

    added initial launch file..

--- ltm/main.cpp
+++ ltm/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     ROS_DEBUG("Starting Local Task Manager..");
 
-    ros::init(argc, argv, "LTM");
+    ros::init(argc, argv, "local_task_manager");
     ros::NodeHandle nh;
 
     sir::local_task_manager exec(nh);
