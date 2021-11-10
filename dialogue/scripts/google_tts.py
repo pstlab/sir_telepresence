794e345d51ac4d9c44d2cffc3975a95195b27d0c
GloriaB <gloriaberaldo@gmail.com>
2021-11-10 18:30:05 +0100

    Fix the tts on the real robot

--- dialogue/scripts/google_tts.py
+++ dialogue/scripts/google_tts.py
@@ -3,7 +3,7 @@ import rospy
 import gtts
 from playsound import playsound
 from msgs.srv import set_string, set_stringResponse
-
+import os #adding alternatively to playsound 
 
 class text_to_speech:
 
@@ -15,7 +15,8 @@ class text_to_speech:
         rospy.logdebug('synthesizing "%s"..', req.text)
         tts = gtts.gTTS(req.text, lang='it')
         tts.save('utterance.mp3')
-        playsound('utterance.mp3')
+        #playsound('utterance.mp3')
+        os.system("mpg123 " + "utterance.mp3")
         return set_stringResponse(True)
 
 
@@ -25,3 +26,4 @@ if __name__ == '__main__':
 
     tts = text_to_speech()
     rospy.spin()
+~                
