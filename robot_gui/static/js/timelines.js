b9da005326274072b3401236060efc00aa240fe5
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2021-12-08 16:53:32 +0100

    minor improvements..

--- robot_gui/static/js/timelines.js
+++ robot_gui/static/js/timelines.js
@@ -25,6 +25,27 @@ set_face_service.advertise(function (request, response) {
 var reasoner_created_listener = new ROSLIB.Topic({ ros: ros, name: '/reasoner_created', messageType: 'std_msgs/UInt64' });
 reasoner_created_listener.subscribe(function (message) {
     console.log('Reasoner ' + message.data + ' was created..');
+
+    const reasoners_tabs = document.getElementById('reasoners-tabs');
+    const reasoner_tab_template = document.getElementById('reasoner-tab-template');
+    const reasoner_tab = reasoner_tab_template.content.cloneNode(true).querySelector('li');
+    const reasoner_button = reasoner_tab.querySelector('button');
+    if (!reasoners_tabs.hasChildNodes())
+        reasoner_button.classList.add('active');
+    reasoner_button.setAttribute('id', 'r' + message.data + '-tab');
+    reasoner_button.setAttribute('data-bs-target', '#r' + message.data);
+    reasoner_button.setAttribute('aria-controls', 'r' + message.data);
+    reasoner_button.append('Reasoner (' + message.data + ')');
+    reasoners_tabs.append(reasoner_tab);
+
+    const reasoners_content = document.getElementById('reasoners-content');
+    const reasoner_template = document.getElementById('reasoner-content-template');
+    const reasoner = reasoner_template.content.cloneNode(true).querySelector('div');
+    if (!reasoners_content.hasChildNodes())
+        reasoner.classList.add('show', 'active');
+    reasoner.setAttribute('id', 'r' + message.data);
+    reasoner.setAttribute('aria-labelledby', 'r' + message.data + '-tab');
+    reasoners_content.append(reasoner);
 });
 
 var reasoner_destroyed_listener = new ROSLIB.Topic({ ros: ros, name: '/reasoner_destroyed', messageType: 'std_msgs/UInt64' });
@@ -39,32 +60,40 @@ flaw_created_listener.subscribe(function (message) {
 
 var flaw_state_changed_listener = new ROSLIB.Topic({ ros: ros, name: '/flaw_state_changed', messageType: 'deliberative_tier/flaw_state_changed' });
 flaw_state_changed_listener.subscribe(function (message) {
+    console.log('flaw_state_changed: ' + message);
 });
 
 var flaw_cost_changed_listener = new ROSLIB.Topic({ ros: ros, name: '/flaw_cost_changed', messageType: 'deliberative_tier/flaw_cost_changed' });
 flaw_cost_changed_listener.subscribe(function (message) {
+    console.log('flaw_cost_changed: ' + message);
 });
 
 var flaw_position_changed_listener = new ROSLIB.Topic({ ros: ros, name: '/flaw_position_changed', messageType: 'deliberative_tier/flaw_position_changed' });
 flaw_position_changed_listener.subscribe(function (message) {
+    console.log('flaw_position_changed: ' + message);
 });
 
 var current_flaw_listener = new ROSLIB.Topic({ ros: ros, name: '/current_flaw', messageType: 'deliberative_tier/current_flaw' });
 current_flaw_listener.subscribe(function (message) {
+    console.log('current_flaw: ' + message);
 });
 
 var resolver_created_listener = new ROSLIB.Topic({ ros: ros, name: '/resolver_created', messageType: 'deliberative_tier/resolver_created' });
 resolver_created_listener.subscribe(function (message) {
+    console.log('resolver_created: ' + message);
 });
 
 var resolver_state_changed_listener = new ROSLIB.Topic({ ros: ros, name: '/resolver_state_changed', messageType: 'deliberative_tier/resolver_state_changed' });
 resolver_state_changed_listener.subscribe(function (message) {
+    console.log('resolver_state_changed: ' + message);
 });
 
 var current_resolver_listener = new ROSLIB.Topic({ ros: ros, name: '/current_resolver', messageType: 'deliberative_tier/current_resolver' });
 current_resolver_listener.subscribe(function (message) {
+    console.log('current_resolver: ' + message);
 });
 
 var causal_link_added_listener = new ROSLIB.Topic({ ros: ros, name: '/causal_link_added', messageType: 'deliberative_tier/causal_link_added' });
 causal_link_added_listener.subscribe(function (message) {
+    console.log('causal_link_added: ' + message);
 });
